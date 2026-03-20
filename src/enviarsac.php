d7a29e2f247fab1302ab7ca8dc7972a2eb5f6f90
JaqueSantos23 <jaqueline.oliveirasantos@uni9.edu.br>
2026-03-20 14:32:19 +0000

    20/03/26

--- src/enviarsac.php
+++ src/enviarsac.php
@@ -1,8 +1,16 @@
 <h1>Recebe</h1>
 <?php
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+echo "<pre>"; print_r($_POST); echo "</pre>";
+
+$Nome=$_POST["nome"];
+$Email=$_POST["email"];
+$Assunto=$_POST["assunto"];
+$Mensagem=$_POST["mensagem"];
+
+echo "Nome: <b>$nome</b><br>";
+echo "Email: <b>$email</b><br>";
+echo "Assunto: <b>$assunto</b><br>";
+echo "Mensagem: <b>$mensagem</b><br>";
 ?>
 
